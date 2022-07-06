ebcf2e3e9240db4830391d2643e1468d5c4a92ed
rStar <starlove124760@gmail.com>
2022-07-06 14:07:58 +0800

    import asset for character

---
+++ Assets/Enemy Galore 1 - Pixel Art/Script/EnemySelector.cs
@@ -0,0 +1,78 @@
+﻿#region
+
+using UnityEngine;
+using UnityEngine.UI;
+
+#endregion
+
+public class EnemySelector : MonoBehaviour
+{
+#region Private Variables
+
+    [Header("Buttons")]
+    [SerializeField]
+    private Button[] allAnimationButtons;
+
+    [SerializeField]
+    private Button[] enableAnimationButtons;
+
+    [Header("Game Objects")]
+    [SerializeField]
+    private GameObject[] allEnemyGOs;
+
+    [SerializeField]
+    private GameObject enableEnemy;
+
+    [Header("Text")]
+    [SerializeField]
+    private Text enemyName;
+
+#endregion
+
+#region Unity events
+
+    public void Start()
+    {
+        enemyName.text = "";
+    }
+
+#endregion
+
+#region Public Methods
+
+    public void _DisableAllButtons()
+    {
+        for (var i = 0 ; i < allAnimationButtons.Length ; i++) allAnimationButtons[i].interactable = false;
+    }
+
+    public void _DisableAllEnemies()
+    {
+        for (var i = 0 ; i < allEnemyGOs.Length ; i++) allEnemyGOs[i].gameObject.SetActive(false);
+    }
+
+    public void _EnableButton()
+    {
+        for (var i = 0 ; i < enableAnimationButtons.Length ; i++) enableAnimationButtons[i].interactable = true;
+    }
+
+    public void _EnableEnemy()
+    {
+        enableEnemy.SetActive(true);
+    }
+
+    public void _Rename()
+    {
+        enemyName.text = enableEnemy.gameObject.name;
+    }
+
+    public void ChangeEnemies()
+    {
+        _DisableAllEnemies();
+        _DisableAllButtons();
+        _EnableEnemy();
+        _EnableButton();
+        _Rename();
+    }
+
+#endregion
+}
---
+++ Assets/Enemy Galore 1 - Pixel Art/Script/EnemyTest.cs
@@ -0,0 +1,127 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+public class EnemyTest : MonoBehaviour
+{
+#region Private Variables
+
+    [SerializeField]
+    private Animator[] EnemyAnims;
+
+#endregion
+
+#region Public Methods
+
+    public void Animation_1_Idle()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is Idling");
+            }
+    }
+
+    public void Animation_2_Run()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is Running");
+            }
+    }
+
+    public void Animation_3_Hit()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                EnemyAnims[i].SetTrigger("Hit");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is being Hit");
+            }
+    }
+
+    public void Animation_4_Death()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                EnemyAnims[i].SetTrigger("Death");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " has died");
+            }
+    }
+
+    public void Animation_5_Ability()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" ,     false);
+                EnemyAnims[i].SetBool("Ability" , true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its First Ability");
+            }
+    }
+
+    public void Animation_5_Ability2()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" ,       false);
+                EnemyAnims[i].SetBool("Ability 2" , true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its Second Ability");
+            }
+    }
+
+    public void Animation_5_Ability3()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" ,       false);
+                EnemyAnims[i].SetBool("Ability 3" , true);
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using its Third Ability");
+            }
+    }
+
+    public void Animation_6_Attack()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                EnemyAnims[i].SetTrigger("Attack");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Primary Attack");
+            }
+    }
+
+    public void Animation_7_Attack2()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                EnemyAnims[i].SetTrigger("Attack 2");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Secondary Attack");
+            }
+    }
+
+    public void Animation_8_Attack3()
+    {
+        for (var i = 0 ; i < EnemyAnims.Length ; i++)
+            if (EnemyAnims[i].gameObject.activeSelf)
+            {
+                EnemyAnims[i].SetBool("Run" , false);
+                EnemyAnims[i].SetTrigger("Attack 3");
+                Debug.Log("The enemy " + EnemyAnims[i].gameObject.name + " is using using its Tertiary Attack");
+            }
+    }
+
+#endregion
+}

5543517ecaa09f632f762b13a6d51eb6bf2cb54a
rStar <starlove124760@gmail.com>
2022-07-07 19:34:04 +0800

    dispose when despawn

--- Assets/Modules/Domains/Skill/Core/Skill.cs
+++ Assets/Modules/Domains/Skill/Core/Skill.cs
@@ -76,6 +76,7 @@ namespace rStar.Modules.Skill.Core
 
         public void OnDespawned()
         {
+            Dispose();
             pool = null;
             skillRegistry.RemoveSkill(this);
         }

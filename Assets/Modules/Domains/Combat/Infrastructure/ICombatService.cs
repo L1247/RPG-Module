88e96bf1d77da9854cf06f43f54606bc60692e46
rStar <starlove124760@gmail.com>
2022-08-07 20:24:03 +0800

    modify summary

--- Assets/Modules/Domains/Combat/Infrastructure/ICombatService.cs
+++ Assets/Modules/Domains/Combat/Infrastructure/ICombatService.cs
@@ -7,7 +7,7 @@ namespace rStar.RPGModules.Combat.Infrastructure
         /// <summary>
         ///     給予指定Owner傷害
         /// </summary>
-        /// <param name="ownerId">will be hurt stat owner's id</param>
+        /// <param name="ownerId">will be hurt stat by owner's id</param>
         /// <param name="damageAmount">should be positive number</param>
         /// <returns>command was successful</returns>
         bool DealDamage(string ownerId , int damageAmount);

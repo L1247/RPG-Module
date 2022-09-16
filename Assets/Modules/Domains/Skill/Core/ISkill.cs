4c72aeac7c1c1ca79a19aed22d5fc71ab88ee683
rStar <starlove124760@gmail.com>
2022-09-16 12:15:44 +0800

    remove method from interface

--- Assets/Modules/Domains/Skill/Core/ISkill.cs
+++ Assets/Modules/Domains/Skill/Core/ISkill.cs
@@ -10,11 +10,7 @@ namespace rStar.RPGModules.Skill.Core
     {
     #region Public Methods
 
-        void EnterCast();
-        void EnterCd();
         void Execute();
-        void ExitCast();
-        void ExitCd();
         void Init(string ownerId , string dataId , float cast , float cd);
         void Tick(float  time);
         void UseSkill();
